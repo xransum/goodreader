2dfc4ae4acd20067cc84675477a4ede4418adcb8
xransum <xransum@users.noreply.github.com>
2026-01-21 18:38:02 -0500

    Added goodreader init.

---
+++ src/goodreader/__init__.py
@@ -0,0 +1,15 @@
+"""Init file for the package."""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import (  # type: ignore[assignment]
+        PackageNotFoundError,
+        version,
+    )
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
